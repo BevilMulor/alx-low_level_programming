a9bca42bff9dab38d9ebc2699203e5e4c1bd0234
BevilMulor <b.mulor@alueducation.com>
2022-09-21 17:18:14 +0000

    infinite

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
- *main - entry point.
+ *main - print the letters of the alphabet
+ *
+ *Description: print the letters of the alphabet except e, q
+ *
  *Return: Always 0 (Success)
  */
 
 int main(void)
-{
-int i;
+{ int i = 97;
 
-for (i = 48; i < 58; i++)
+while (i < 123)
 {
+if (i != 101 && i != 113)
+{
+											
 putchar(i);
-if (i != 57)
-												{
-												putchar(',');
-												putchar(' ');
 												}
+i++;
 }
-putchar('\n');
+putchar(10);
+
 return (0);
 }
