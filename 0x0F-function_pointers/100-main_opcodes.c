7f03ff7e5eaf0e416be1545d0f248dc06e60d856
BevilMulor <b.mulor@alustudent.com>
2022-10-12 18:24:30 -0700

    completed tasks

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,3 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,7 +5,7 @@
  *main - prints its own opcodes
  *@argc: number of arguments
  *@argv: array of arguments
- *     
+ *    
  *Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
