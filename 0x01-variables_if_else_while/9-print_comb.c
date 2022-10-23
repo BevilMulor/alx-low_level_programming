d489fc2fb2c797bee28ed0150b223f1c28ff2e35
BevilMulor <b.mulor@alustudent.com>
2022-10-23 02:25:11 -0700

    completed tasks

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-
 /**
- *main - print the letters of the alphabet
- *
- *Description: print the letters of the alphabet except e, q
- *
+ *main - entry point.
  *Return: Always 0 (Success)
  */
 
 int main(void)
-{ int i = 97;
-
-while (i < 123)
 {
-if (i != 101 && i != 113)
-{
-											
-putchar(i);
-												}
-i++;
-}
-putchar(10);
+	int i;
 
-return (0);
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
