c573c6018638a412fe724a5365ebe7a6243fbb00
BevilMulor <109423584+BevilMulor@users.noreply.github.com>
2023-02-04 01:15:12 +0000

    dynamic lybraries

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,30 +1,28 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: hash table to look into
- * @key: key of the value to retrieve
- *
- * Return: the value associated with the element,
- * or NULL if key couldn’t be found
+ * hash_table_get - retrieve the value associated with
+ *                  a key in a hash table.
+ * @ht: pointer to the hash table.
+ * @key: key to get the value of.
+ * Return: if the key cannot be matched - NULL.
+ *         otherwise - the value associated with key in ht.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
 	unsigned long int index;
-	hash_node_t *node = NULL;
 
-	if (!ht || !key || !strcmp(key, ""))
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	node = (ht->array)[index];
+	if (index >= ht->size)
+		return (NULL);
 
-	while (node)
-	{
-		if (!strcmp(node->key, (char *)key))
-			return (node->value);
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
-	}
 
-	return (NULL);
+	return ((node == NULL) ? NULL : node->value);
 }
